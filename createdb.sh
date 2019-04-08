8598abc232427b3ecc1bd9dbe8b0c6bac4b34476
Pimax1 <aime.pierre@gmail.com>
2019-04-08 17:34:25 +0800

    adding createdb script

--- createdb.sh
+++ createdb.sh
@@ -6,12 +6,14 @@ set -e
 
 cd ~/git/DrQA/
 export CLASSPATH=~/git/DrQA/data/corenlp/*
+export WKDIR=~/git/DrQA/data/$1/
 
-rm -rf ~/git/DrQA/data/$1/ || true
-mkdir -p ~/git/DrQA/data/$1/extract/
+rm -rf ${WKDIR} || true
+mkdir -p ${WKDIR}extract
 
 echo "download db dump"
-wget -P ~/git/DrQA/data/$1/ $2
+wget -P ${WKDIR} $2
+unzip ${WKDIR}*
 
 #https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream.xml.bz2 Current revisions only, no talk or user pages; this is probably what you want, and is approximately 14 GB compressed (expands to over 58 GB when decompressed).
 #https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-abstract.xml.gz – page abstracts #page abstracts
@@ -19,16 +21,16 @@ wget -P ~/git/DrQA/data/$1/ $2
 #https://s3.amazonaws.com/wikia_xml_dumps/s/sh/shadowsdietwice_pages_current.xml.7z   https://sekiro-shadows-die-twice.fandom.com/wiki/Special:Statistics
 
 echo "wikiextractor"
-python3  WikiExtractor.py --output ~/git/DrQA/data/$1/extract/ --json --no-templates --min_text_length 3 --filter_disambig_pages --processes 18 ~/git/DrQA/data/$1/
+python3  WikiExtractor.py --output ${WKDIR}extract/ --json --no-templates --min_text_length 3 --filter_disambig_pages --processes 18 ${WKDIR}*.xml
 
 echo "building db"
-python build_db.py ~/git/DrQA/data/$1/extract/ ~/git/DrQA/data/$1/docs.db
+python build_db.py ${WKDIR}extract/ ${WKDIR}docs.db
 
 echo "building TF"
-python3 scripts/retriever/build_tfidf.py ~/git/DrQA/data/$1/docs.db ~/git/DrQA/data/$1/ --num-workers 18 --ngram 2 --hash-size 16777216 --tokenizer 'simple'
+python3 scripts/retriever/build_tfidf.py ${WKDIR}docs.db ${WKDIR} --num-workers 18 --ngram 2 --hash-size 16777216 --tokenizer 'simple'
 
 echo "exporting to storage"
-gsutil cp ~/git/DrQA/data/$1/docs.db gs://pimax/drqa/data/$1/docs.db
-gsutil cp ~/git/DrQA/data/$1/docs-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz gs://pimax/drqa/data/$1/docs-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz
+gsutil cp ${WKDIR}docs.db gs://pimax/drqa/data/$1/docs.db
+gsutil cp ${WKDIR}docs-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz gs://pimax/drqa/data/$1/docs-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz
 
 echo "done"
