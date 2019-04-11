de93af7ca71bea2f1348afc6057e052521a7ad0e
Pimax1 <aime.pierre@gmail.com>
2019-04-11 23:57:15 +0800

    test

--- getdb.sh
+++ getdb.sh
@@ -7,11 +7,11 @@ set -e
 DBPATH=~/git/DrQA/data/$1/docs.db
 TFPATH=~/git/DrQA/data/$1/docs-tfidf-ngram=2-hash=16777216-tokenizer=corenlp.npz
 
-if not test -f "DBPATH"; then
+if not test -f "$DBPATH"; then
     gsutil cp  gs://pimax/drqa/data/$1/docs.db $DBPATH
 fi
 
-if not test -f "TFPATH"; then
+if not test -f "$TFPATH"; then
     gsutil cp  gs://pimax/drqa/data/$1/docs-tfidf-ngram=2-hash=16777216-tokenizer=corenlp.npz $TFPATH
 fi
 
