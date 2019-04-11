5824924c1f869b380e3730e410d6bbaf9d6dffc8
Pimax1 <aime.pierre@gmail.com>
2019-04-12 00:06:33 +0800

    test

--- getdb.sh
+++ getdb.sh
@@ -8,10 +8,12 @@ DBPATH=~/git/DrQA/data/$1/docs.db
 TFPATH=~/git/DrQA/data/$1/docs-tfidf-ngram=2-hash=16777216-tokenizer=corenlp.npz
 
 if not test -f "$DBPATH"; then
+    echo "download db"
     gsutil cp  gs://pimax/drqa/data/$1/docs.db $DBPATH
 fi
 
 if not test -f "$TFPATH"; then
+    echo "download tf"
     gsutil cp  gs://pimax/drqa/data/$1/docs-tfidf-ngram=2-hash=16777216-tokenizer=corenlp.npz $TFPATH
 fi
 
