9782991d428e885299617374cd11e8c2385b2b42
Pimax1 <aime.pierre@gmail.com>
2019-04-16 04:52:49 +0800

    test

--- createdb.sh
+++ createdb.sh
@@ -8,8 +8,8 @@ cd ~/git/DrQA/
 export CLASSPATH=~/git/DrQA/data/corenlp/*
 export WKDIR=~/git/DrQA/data/$1/
 
-rm -rf ${WKDIR} || true
-mkdir -p ${WKDIR}extract
+#rm -rf ${WKDIR} || true
+#mkdir -p ${WKDIR}extract
 
 echo "download db dump"
 wget --timestamping --directory-prefix=${WKDIR} $2
