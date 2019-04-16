250f635f5b410c038a401ff157adab0f42c2bb99
Pimax1 <aime.pierre@gmail.com>
2019-04-16 18:22:47 +0800

    test

--- createdb.sh
+++ createdb.sh
@@ -29,7 +29,7 @@ cd ~/git/DrQA/
 
 if [ "$1" != "wikiabstract" ]; then
     echo "wikiextractor"
-    #python WikiExtractor.py --output ${WKDIR}extract/ --json --no-templates --min_text_length 15 --filter_disambig_pages --processes 18 ${WKDIR}*xml*
+    python WikiExtractor.py --output ${WKDIR}extract/ --json --no-templates --min_text_length 15 --filter_disambig_pages  ${WKDIR}*.bz2
 else
     echo "abstractor"
     python3 abstractor.py --input ${WKDIR}enwiki-latest-abstract.xml --output ${WKDIR}extract/
